3308a694084ea9c23339edb9bf71519d087b5c5a
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-01-09 12:43:55 -0600

    added delete function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
+var pageContentEl = document.querySelector("#page-content");
 
 var taskFormHandler = function(event) {
   event.preventDefault();
@@ -96,5 +97,21 @@ var createTaskActions = function(taskID) {
   return actionContainerEl;
 
 }
-
 formEl.addEventListener("submit", taskFormHandler);
+
+
+var taskButtonHandler = function(event) {
+  console.log(event.target);
+
+  if (event.target.matches(".delete-btn")) {
+    // get the element's task id
+    var taskId = event.target.getAttribute("data-task-id");
+    deleteTask(taskId);
+  }
+};
+pageContentEl.addEventListener("click", taskButtonHandler);
+
+var deleteTask = function(taskId) {
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+  taskSelected.remove();
+};
