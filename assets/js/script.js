05beca8bdb5db83de0ed5689bfb584fa16134cc6
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-01-09 13:41:45 -0600

    finished status change function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
 var pageContentEl = document.querySelector("#page-content");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 
 var taskFormHandler = function(event) {
   event.preventDefault();
@@ -113,7 +115,6 @@ var createTaskActions = function(taskID) {
   return actionContainerEl;
 
 }
-formEl.addEventListener("submit", taskFormHandler);
 
 
 // function for task buttons
@@ -132,7 +133,6 @@ var taskButtonHandler = function(event) {
     deleteTask(taskId);
   }
 };
-pageContentEl.addEventListener("click", taskButtonHandler);
 
 
 // function for delete task
@@ -173,3 +173,33 @@ var completeEditTask = function(taskName, taskType, taskId) {
   formEl.removeAttribute("data-task-id");
   document.querySelector("#save-task").textContent = "Add Task";
 };
+
+
+// function for status change
+var taskStatusChangeHandler = function(event) {
+
+  // get the task item's id
+  var taskId = event.target.getAttribute("data-task-id");
+
+  // get the currently selected option's value and convert to lowercase
+  var statusValue = event.target.value.toLowerCase();
+
+  // find the parent task item element based on the id
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+  if (statusValue === "to do") {
+    tasksToDoEl.appendChild(taskSelected);
+  }
+  else if (statusValue === "in progress") {
+    tasksInProgressEl.appendChild(taskSelected);
+  }
+  else if (statusValue === "completed") {
+    tasksCompletedEl.appendChild(taskSelected);
+  }
+};
+
+
+// event listeners
+formEl.addEventListener("submit", taskFormHandler);
+pageContentEl.addEventListener("click", taskButtonHandler);
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
